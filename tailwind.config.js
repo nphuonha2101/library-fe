508e74f0137a443e6c8b06f19f58e42cc3b50b1d
PhuongCute <bichphuonglaithi06032003@gmail.com>
2024-08-04 17:22:14 +0700

    3 - feat: create interface add css postcss config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+import lineClamp from '@tailwindcss/line-clamp';
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        'custom': '10px',
+      },
+
+    },
   },
-  plugins: [],
+  plugins: [
+      typography,
+      lineClamp,
+    ],
 }
