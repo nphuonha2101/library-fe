c3bef9452c9cac5a11f447c629f2788203029ef6
Phuong Nha Nguyen <nphuonha2101@gmail.com>
2024-08-12 11:58:24 +0700

    feat: create Header and Footer

--- src/components/layouts/Master.tsx
+++ src/components/layouts/Master.tsx
@@ -1,11 +1,14 @@
+import { Footer } from "../paritials/Footer/Footer"
+import { Header } from "../paritials/Header/Header"
+
 export const Master = ({ children }: { children?: React.ReactNode }) => {
     return (
         <>
-            {/* <Header /> */}
-
-            {children}
-
-            {/* <Footer /> */}
+            <Header />
+            <section className="container mx-auto w-full mt-6">
+                {children}
+            </section>
+            <Footer />
         </>
-    )
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+import flowbitePlugin from 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'calc-100vw': 'calc(100vw)',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    flowbitePlugin
+  ],
 }
