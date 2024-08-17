0301c8e7d242f36db95d1ffb20a39bd30e0608b0
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-08-17 23:06:51 +0700

    12 - feat: add modal edit book

--- src/components/layouts/Admin.tsx
+++ src/components/layouts/Admin.tsx
@@ -109,7 +109,56 @@ export const Admin = () => {
                                                 <span className="sr-only">Close modal</span>
                                             </button>
                                         </div>
-
+                                        <form className="p-4 md:p-5">
+                                            <div className="grid gap-4 mb-4 grid-cols-2">
+                                                <div className="col-span-2">
+                                                    <label htmlFor="name"
+                                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tên
+                                                        sách</label>
+                                                    <input type="text" name="name" id="name"
+                                                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                                           placeholder="Nhập tên sách"/>
+                                                </div>
+                                                <div className="col-span-2 sm:col-span-1">
+                                                    <label htmlFor="price"
+                                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Số
+                                                        lượng</label>
+                                                    <input type="number" name="price" id="price"
+                                                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                                           placeholder="1"/>
+                                                </div>
+                                                <div className="col-span-2 sm:col-span-1">
+                                                    <label htmlFor="category"
+                                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Thể
+                                                        loại</label>
+                                                    <select id="category"
+                                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                        <option>Chọn thể loại</option>
+                                                        <option value="PC">PC</option>
+                                                        <option value="GA">Gaming/Console</option>
+                                                        <option value="PH">Phones</option>
+                                                    </select>
+                                                </div>
+                                                <div className="col-span-2">
+                                                    <label htmlFor="description"
+                                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mô
+                                                        tả</label>
+                                                    <textarea id="description"
+                                                              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                              placeholder="Nhập mô tả ở đây"></textarea>
+                                                </div>
+                                            </div>
+                                            <button type="submit"
+                                                    className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor"
+                                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd"
+                                                          d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                                          clip-rule="evenodd"></path>
+                                                </svg>
+                                                Thêm sách mới
+                                            </button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
