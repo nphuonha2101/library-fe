8e776c849d7a94a91730fb5d61d48310038d9591
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-08-21 11:22:34 +0700

    12 - fix: book detail

--- src/components/pages/BookDetail/BookDetail.tsx
+++ src/components/pages/BookDetail/BookDetail.tsx
@@ -11,7 +11,7 @@ export const BookDetail = () => {
     useEffect(() => {
         // Replace with your actual fetch logic
         const fetchBook = async () => {
-            const response = await fetch(`https://freetestapi.com/api/v1/books/1`);
+            const response = await fetch(`https://localhost:7259/api/v1/books/${id}`);
             const data: IBookItem = await response.json();
             setBook(data);
         };
@@ -26,8 +26,9 @@ export const BookDetail = () => {
     function incrementQuantity() {
         setQuantity(quantity + 1);
     }
+
     function decrementQuantity() {
-        if(quantity===0){
+        if (quantity === 0) {
             return;
         }
         setQuantity(quantity - 1);
@@ -39,7 +40,7 @@ export const BookDetail = () => {
             <div className="col-span-1 max-w-[460px] bg-white rounded-lg shadow-md p-4">
                 <div>
                     <img
-                        src={"https://images.unsplash.com/photo-1723441857662-d465a52e78d0?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                        src={book.bookImage}
                         alt={book.title} className="w-full rounded-lg"/>
                 </div>
                 <div className="mt-4">
